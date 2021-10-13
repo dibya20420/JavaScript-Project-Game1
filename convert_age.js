3e45630490997d96f40cc9a764acd6acdf7454f7
dibya20420 <dibya.ranjan031@gmail.com>
2021-10-13 14:38:05 +0530

    game 1 is completed but it is not working

---
+++ convert_age.js
@@ -0,0 +1,12 @@
+function ageInDays(){
+    var  birthYear = prompt('what is the year in which you born');
+    var ageIndays = (2021-birthYear)*365;
+    var h1 = document.createElement('h1');
+    var textAns = document.createTextNode('you are' + ageIndays +'days old');
+    h1.setAttribute('id','ageInDays');
+    h1.appendChild(textAns);
+    document.createElement('flex-box-result').appendChild(h1);
+}
+function reset(){
+    document.getElementById('flex-box-result').remove();
+}
